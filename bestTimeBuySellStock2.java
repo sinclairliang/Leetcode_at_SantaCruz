94b9d757741f447e630fb575cdc2d9e9f9c98d19
ablimm <32781736+ablimm@users.noreply.github.com>
2018-09-29 16:41:20 -0700

    Update bestTimeBuySellStock2.java
    cleaned

--- bestTimeBuySellStock2.java
+++ bestTimeBuySellStock2.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxProfit(int[] prices) {
         if(prices==null || prices.length < 2) return 0;
         int profit = 0;
-        int j = 1;
+
         for(int i = 1 ; i< prices.length; i++){
             if(prices[i] > prices[i-1]){
                 profit += prices[i]-prices[i-1];
