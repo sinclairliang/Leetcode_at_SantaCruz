0274d7ae8c12b506db5e62dd8557bbd6d650ab04
Sinclair Liang <sinclairliang@gmail.com>
2018-10-04 20:10:01 -0700

    added remove nth node from the end java

---
+++ linked_list/remove_nth_from_end.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        // using two pointer technique;
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast!= null)
+        {
+            // maintaining the gap to be n;
+            // let fast reach the end of the linked list;
+            // slow will be the one we want to delete;
+            if(n < 0)
+            {
+                slow = slow.next;
+            }
+            n--;
+            // now fast is n nodes ahead of slow;
+            fast = fast.next;
+        }
+        
+        if(n<0)
+        {
+            // in this case, we actually minused n; 
+            slow.next = slow.next.next;
+        }
+        else
+        {
+            // otherwise, we just remove the first node;
+            head = head.next;
+        }
+        
+        return head;
+        
+        
+    }
+}
