21064b2eb188bde3895bbf5a7a166bfe2f0c0e5a
Sinclair Liang <sinclairliang@gmail.com>
2018-10-12 01:33:49 -0700

    recursively solved

---
+++ tree/Symmetric_tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // recursive
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+        {
+            return true;
+        }
+        return helper(root.left, root.right);
+        
+    }
+    
+    boolean helper(TreeNode left, TreeNode right)
+    {
+        if(left==null || right==null)
+        {
+            return (left==right);
+        }
+        if(left.val!=right.val)
+        {
+            return false;
+        }
+        return (helper(left.left, right.right) && helper(left.right, right.left));
+    }
+}
