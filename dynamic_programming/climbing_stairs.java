a692ef34227b9152c24e0dbb0ce4453f9d50a602
Thanut Art Parkeenvincha <art.parkeenvincha@gmail.com>
2018-10-13 15:38:58 -0700

    Climbing stairs (added comments)

--- dynamic_programming/climbing_stairs.java
+++ dynamic_programming/climbing_stairs.java
@@ -1,17 +1,25 @@
 class Solution {
     public int climbStairs(int n) {
+        //Base cases
         if(n == 0) {return 0;}
         if(n == 1) {return 1;}
         if(n == 2) {return 2;}
+
+        //How many ways can you get to the ith level = opt[i]
         int[] opt = new int[n];
+
+        //Base cases
         opt[0] = 1;
         opt[1] = 2;
         
+        //Compute the maximum number of ways to get to the ith level
         for(int i = 2; i < n; i++)
         {
+            //Either we take 1 step up from the (i-1)th level, or we take 2 steps up from the (i-2)th level
             opt[i] = opt[i-1] + opt[i-2];
         }
         
+        //Return the maximum number of ways to get to level n
         return opt[n-1];
     }
 }
