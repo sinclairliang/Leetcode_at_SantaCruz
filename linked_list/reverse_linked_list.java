741c310a4dc0517ff37678a99e6482f109a727a9
Sinclair Liang <sinclairliang@gmail.com>
2018-10-14 11:56:22 -0700

    reverse Linkedlist

---
+++ linked_list/reverse_linked_list.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // https://www.geeksforgeeks.org/reverse-a-linked-list/
+        ListNode curr = null;
+        ListNode prev = null;
+        ListNode next = null;
+        curr = head;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
