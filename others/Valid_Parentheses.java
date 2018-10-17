dc6b4be7ff1e7d2b54e55cab6fa5db62a435825e
Sinclair Liang <sinclairliang@gmail.com>
2018-10-17 03:08:26 -0700

    using stack()

---
+++ others/Valid_Parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    // by using stack to solve
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char [] parentheses = s.toCharArray();
+        for(int i=0; i<parentheses.length; i++)
+        {
+            // traverse the whole char array;
+            if(parentheses[i] == '(' || parentheses[i] == '[' || parentheses[i] == '{')
+            {
+                // push to stack if opening parentheses;
+                stack.push(parentheses[i]);
+            }
+            else
+            {
+                if(stack.empty())
+                {
+                    // edge case: start by closing ones;
+                    return false;
+                }
+                char to_compare = stack.peek();
+                if(to_compare == '(' && parentheses[i] == ')' || to_compare == '[' && parentheses[i] == ']' || to_compare == '{' && parentheses[i] == '}')
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return (stack.empty());
+
+    }
+}
