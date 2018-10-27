3b3ee06a7051a830e1c48f67bffb6233a6d56ff7
Sinclair Liang <sinclairliang@gmail.com>
2018-10-27 12:24:26 -0700

    autoformat

--- easy/linked_list/linked_list_cycle_ii.java
+++ easy/linked_list/linked_list_cycle_ii.java
@@ -1,42 +1,35 @@
 /**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
+ * Definition for singly-linked list. class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         // use mathematical knowledge to solve this puzzle;
-        if(head == null || head.next == null) {
+        if (head == null || head.next == null) {
             // checking edge cases;
             return null;
         }
-        
+
         ListNode fast = head;
         ListNode slow = head;
         boolean flag = false;
-        while(fast != null && fast.next != null) {
+        while (fast != null && fast.next != null) {
             // build on Catherine's answer: first to check if there is a cycle;
             fast = fast.next.next;
             slow = slow.next;
-            if(fast == slow) {
+            if (fast == slow) {
                 // if there is, mark flag true;
                 flag = true;
                 break;
             }
         }
-        
-        if(!flag) {
+
+        if (!flag) {
             // if while loop broke, flag is false,
             // means there is no cycle;
             return null;
         }
-        
+
         // cool, now there is a cycle, how can I find the beginning node of that cycle?
         // look at this illustration: https://postimg.cc/TySKb2PV
         // fast and slow meet at z
@@ -50,7 +43,7 @@ public class Solution {
         // let dummyhead and slow travel the same amout of distance
         // they will meet again at exact Y, the beginning of cycle;
         ListNode dummyhead = head;
-        while(dummyhead != slow) {
+        while (dummyhead != slow) {
             dummyhead = dummyhead.next;
             slow = slow.next;
         }
