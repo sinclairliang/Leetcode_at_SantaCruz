a265c50e40eb6f64268aebae9ac047e5542ff2e2
Sinclair Liang <sinclairliang@gmail.com>
2019-06-17 21:13:20 -0500

    guys, I missed the days we code together

--- frequentQuestions/two_sum.java
+++ frequentQuestions/two_sum.java
@@ -1,3 +1,4 @@
+
 // class Solution {
 //     public int[] twoSum(int[] nums, int target) {
 //         // used a brute force method to solve;
@@ -18,20 +19,21 @@
 // }
 
 import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap <Integer, Integer> map = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         int complement;
-        
-        for(int i=0; i<nums.length; i++) {
+
+        for (int i = 0; i < nums.length; i++) {
             complement = target - nums[i];
-            if(map.containsKey(complement)){
-                return new int[] {i, map.get(complement)};
+            if (map.containsKey(complement)) {
+                return new int[] { i, map.get(complement) };
             }
-            
+
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No Solution");
-        
+
     }
 }
