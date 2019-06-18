418ae99cf8625cb5d672181b491915a4ee376740
Sinclair Liang <sinclairliang@gmail.com>
2019-06-17 21:06:08 -0500

    used hashmap now

--- easy/arrays/two_sum.java
+++ easy/arrays/two_sum.java
@@ -1,18 +1,37 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         // used a brute force method to solve;
+//         // it seems there is HashMap method, which is faster;
+//         // but I don't understand;
+//         int[] res = new int[2];
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     res[0] = i;
+//                     res[1] = j;
+//                     return res;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // used a brute force method to solve;
-        // it seems there is HashMap method, which is faster;
-        // but I don't understand;
-        int[] res = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    res[0] = i;
-                    res[1] = j;
-                    return res;
-                }
+        HashMap <Integer, Integer> map = new HashMap<Integer, Integer>();
+        int complement;
+        
+        for(int i=0; i<nums.length; i++) {
+            complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[] {i, map.get(complement)};
             }
+            
+            map.put(nums[i], i);
         }
-        return res;
+        throw new IllegalArgumentException("No Solution");
+        
     }
 }
--- medium/strings/longest_substring_without_repeating_characters.java
+++ medium/strings/longest_substring_without_repeating_characters.java
@@ -1,38 +1,39 @@
 class Solution {
     static final int NO_OF_CHARS = 256;
+
     public int lengthOfLongestSubstring(String s) {
-        if(s.length() == 0 || s == null) {
+        if (s.length() == 0 || s == null) {
             return 0;
         }
-        
+
         int n = s.length();
         int cur_length = 1;
         int max_length = 1;
         int prev_index;
         int i;
-        int visited [] = new int[NO_OF_CHARS];
-        
-        for(i = 0; i < NO_OF_CHARS; i++) {
+        int visited[] = new int[NO_OF_CHARS];
+
+        for (i = 0; i < NO_OF_CHARS; i++) {
             visited[i] = -1;
         }
-        
+
         visited[s.charAt(0)] = 0;
-        
-        for(i = 1; i < n; i++) {
+
+        for (i = 1; i < n; i++) {
             prev_index = visited[s.charAt(i)];
-            
-            if(prev_index == -1 || i - cur_length > prev_index) {
+
+            if (prev_index == -1 || i - cur_length > prev_index) {
                 cur_length++;
             } else {
-                if(cur_length > max_length) {
+                if (cur_length > max_length) {
                     max_length = cur_length;
                 }
                 cur_length = i - prev_index;
             }
             visited[s.charAt(i)] = i;
-            
+
         }
-        if(cur_length > max_length) {
+        if (cur_length > max_length) {
             max_length = cur_length;
         }
         return max_length;
