265aaf5e4adf79ba3b7303700419d1a021c61603
Sinclair Liang <sinclairliang@gmail.com>
2019-06-17 21:50:01 -0500

    added new stuff

--- frequentQuestions/longestPalindrome.java
+++ frequentQuestions/longestPalindrome.java
@@ -1,5 +1,32 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        // TODO
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> countMap = new HashMap<>();
+        // Created a count Map to store frequencies of each character;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (countMap.containsKey(c)) {
+                countMap.put(c, countMap.get(c) + 1);
+            } else {
+                countMap.put(c, 1);
+            }
+
+            // Here we have a character - frequency binding;
+        }
+
+        int answer = 0;
+        for (int count : countMap.values()) {
+            if (count % 2 == 0) {
+                answer += count;
+            } else {
+                answer += count / 2 * 2;
+                if (answer % 2 == 0) {
+                    answer += 1;
+                }
+
+            }
+        }
+        return answer;
     }
 }
