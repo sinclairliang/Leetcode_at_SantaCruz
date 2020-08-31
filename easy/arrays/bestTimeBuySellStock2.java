420a528397ec709c796212ff8c65d185533eb552
Sinclair Liang <sinclairliang@gmail.com>
2020-08-30 23:55:27 -0500

    reformatted

--- easy/arrays/bestTimeBuySellStock2.java
+++ easy/arrays/bestTimeBuySellStock2.java
@@ -1,6 +1,8 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length < 2) return 0;
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
         int profit = 0;
 
         for (int i = 1; i < prices.length; i++) {
